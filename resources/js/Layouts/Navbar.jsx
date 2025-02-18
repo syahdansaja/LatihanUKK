ad50d349ecc3979c9961c09ee071e7311e5001cc
Lenovo <=>
2025-02-18 09:04:02 +0700

    First commit

---
+++ resources/js/Layouts/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "@inertiajs/react";
+export default function Navbar() {
+    return (
+        <>
+        <nav className="flex justify-between px-8 py-4 gap-4 bg-sky-600">
+          <h1 className="gap-2 text-2xl font-extrabold my-auto">Aplikasi Penjualan</h1>
+          <ul className="flex gap-2 justify-start">
+              <li className="px-4 py-2 text-lg hover:[text-shadow:_0_2px_4px_rgb(99_102_241_/_0.8)] hover:text-sky-400 rounded-xl">
+                <Link href="/produk" className="block">Produk</Link>
+              </li>
+              <li className="px-4 py-2 text-lg hover:[text-shadow:_0_2px_4px_rgb(99_102_241_/_0.8)] hover:text-sky-400 rounded-xl">
+                <Link href="/pelanggan" className="block">Pelanggan</Link>
+              </li>
+              <li className="px-4 py-2 text-lg hover:[text-shadow:_0_2px_4px_rgb(99_102_241_/_0.8)] hover:text-sky-400 rounded-xl">
+                <Link href="/penjualan" className="block">Penjualan</Link>
+              </li>
+          </ul>
+        </nav>
+      </>
+    )
+}
